042571a79bb0f506e34ff3bd2e69cdc6accf0dd8
skinylabs <skinylabs@gmail.com>
2024-08-07 16:28:18 +0700

    fix navbar and home #1

--- src/components/Navbar/NavList.tsx
+++ src/components/Navbar/NavList.tsx
@@ -2,11 +2,26 @@ import React from "react";
 import Link from "next/link";
 
 export const links = [
-  { name: "Home", link: "/" },
-  { name: "About Me", link: "#about" },
-  { name: "My Works", link: "#works" },
-  { name: "Skills", link: "#skills" },
-  { name: "Certificates", link: "#certificates" },
+  {
+    name: "Home",
+    link: "/",
+  },
+  {
+    name: "About Me",
+    link: "#about",
+  },
+  {
+    name: "Portfolio",
+    link: "#portfolio",
+  },
+  {
+    name: "Skills",
+    link: "#skills",
+  },
+  {
+    name: "Certificates",
+    link: "#certificates",
+  },
 ];
 const NavList = () => {
   return (
@@ -15,7 +30,7 @@ const NavList = () => {
         <li key={index}>
           <Link
             href={link.link}
-            className="py-7 inline-block hover:text-blue-500 ease-in-out duration-200"
+            className="nav-link h-16 flex justify-center items-center hover:text-blue-500 ease-in-out duration-200"
           >
             {link.name}
           </Link>
--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -3,45 +3,54 @@
 import React, { useState } from "react";
 import { Menu, X } from "lucide-react";
 import { Button } from "@/components/ui/button";
-import MaxWidthWrapper from "../MaxWidthWrapper";
 import NavList from "./NavList"; // Pastikan jalur impor benar
+import { Icons } from "../Icons/icons";
 
 const Navbar = () => {
   const [open, setOpen] = useState(false);
+
   return (
     <nav className="bg-white border-b">
-      <div className="w-full fixed font-medium z-50 ">
-        <MaxWidthWrapper>
-          <div className="flex items-center justify-between ">
-            <div>
-              {/* <Image src={Logo} alt="logo" className="md:cursor-pointer h-9" /> */}
-              <h1 className="md:text-white">Afif Misbahuddin</h1>
+      <div className="w-full  fixed font-medium z-50 px-4">
+        <div className="flex h-16 items-center justify-between">
+          <div>
+            {/* <Image src={Logo} alt="logo" className="md:cursor-pointer h-9" /> */}
+            <Icons.logo className="flex w-10 h-10 fill-white" />
+            {/* <h1 className="md:text-white">Afif Misbahuddin</h1> */}
+          </div>
+          <ul className="lg:flex hidden uppercase items-center gap-8">
+            <NavList />
+          </ul>
+          <div className="flex gap-4 items-center">
+            <div className="md:hidden">
+              <Button variant="default">Contact Me</Button>
             </div>
-            <ul className="md:flex hidden uppercase items-center gap-8">
-              <NavList />
-            </ul>
-            <div className="flex gap-4 items-center">
-              <div className="">
-                <Button variant="default">Contact Me</Button>
-              </div>
-              <div
-                className="text-3xl md:hidden z-20"
-                onClick={() => setOpen(!open)}
-              >
-                {open ? <X /> : <Menu />}
-              </div>
+            <div
+              className="text-3xl lg:hidden z-20"
+              onClick={() => setOpen(!open)}
+            >
+              {open ? <X /> : <Menu />}
             </div>
-            <ul
-              className={`
-              md:hidden bg-white fixed w-full top-0 overflow-y-auto bottom-0 py-24 pl-4
+          </div>
+          <ul
+            className={`
+              lg:hidden bg-white fixed w-full md:w-1/2 top-0 overflow-y-auto bottom-0 py-24 px-4
               duration-500 ${open ? "left-0" : "left-[-100%]"}
             `}
-            >
-              <NavList />
-            </ul>
-          </div>
-        </MaxWidthWrapper>
+          >
+            <NavList />
+            <div>
+              <Button variant="default" className="w-full flex gap-4">
+                Contact Me
+              </Button>
+            </div>
+          </ul>
+        </div>
       </div>
+      {/* Tambahkan elemen untuk efek blur */}
+      {open && (
+        <div className="fixed inset-0 bg-black bg-opacity-50 backdrop-blur-sm lg:hidden z-10"></div>
+      )}
     </nav>
   );
 };
